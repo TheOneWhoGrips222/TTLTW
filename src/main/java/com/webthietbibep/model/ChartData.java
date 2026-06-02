bf73049dc9fb159f768d5329a2e4c39f15127031
Truong Le Thanh <23130356@st.hcmuaf.edu.vn>
2026-06-02 09:54:06 +0700

    Feature_ChartData_thêm field orderCount và productsSold phục vụ tooltip biểu đồ

--- src/main/java/com/webthietbibep/model/ChartData.java
+++ src/main/java/com/webthietbibep/model/ChartData.java
@@ -3,15 +3,25 @@ package com.webthietbibep.model;
 public class ChartData {
     private String date;
     private double value;
+    private int    orderCount;
+    private int    productsSold;
 
     public ChartData() {}
+
     public ChartData(String date, double value) {
-        this.date = date;
+        this.date  = date;
         this.value = value;
     }
-    // Getters Setters
-    public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
-    public double getValue() { return value; }
-    public void setValue(double value) { this.value = value; }
+
+    public String getDate()          { return date; }
+    public void   setDate(String d)  { this.date = d; }
+
+    public double getValue()           { return value; }
+    public void   setValue(double v)   { this.value = v; }
+
+    public int  getOrderCount()        { return orderCount; }
+    public void setOrderCount(int c)   { this.orderCount = c; }
+
+    public int  getProductsSold()      { return productsSold; }
+    public void setProductsSold(int p) { this.productsSold = p; }
 }
