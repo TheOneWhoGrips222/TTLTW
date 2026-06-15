878fe8f2a27899fe618a0bf2ef623134b4e9d250
Tent23 <23130335@st.hcmuaf.edu.vn>
2026-06-15 17:59:05 +0700

    Create_ReviewServlet_ tạo doPost ở Servlet của Review

--- src/main/java/com/webthietbibep/controller/ReviewServlet.java
+++ src/main/java/com/webthietbibep/controller/ReviewServlet.java
@@ -2,6 +2,9 @@ package com.webthietbibep.controller;
 
 import com.webthietbibep.dao.OrdersDAO;
 import com.webthietbibep.dao.ProductDAO;
+import com.webthietbibep.dao.ReviewDao;
+import com.webthietbibep.model.Review;
+import com.webthietbibep.model.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,6 +18,7 @@ public class ReviewServlet extends HttpServlet {
 
     private ProductDAO productDAO = new ProductDAO();
     private OrdersDAO ordersDAO = new OrdersDAO();
+    private ReviewDao reviewDAO = new ReviewDao();
 
     @Override
     protected void doGet(
@@ -40,5 +44,56 @@ public class ReviewServlet extends HttpServlet {
 
         req.getRequestDispatcher("/review.jsp")
                 .forward(req, resp);
+    }
+    @Override
+    protected void doPost(
+            HttpServletRequest req,
+            HttpServletResponse resp)
+            throws IOException {
+
+        User user = (User) req.getSession()
+                .getAttribute("user");
+
+        if(user == null){
+            resp.sendRedirect("login");
+            return;
+        }
+
+        int productId =
+                Integer.parseInt(req.getParameter("productId"));
+
+        int orderId =
+                Integer.parseInt(req.getParameter("orderId"));
+
+        int rating =
+                Integer.parseInt(req.getParameter("rating"));
+
+        String comment =
+                req.getParameter("comment");
+
+        if(rating < 1 || rating > 5){
+            resp.sendRedirect("orders");
+            return;
+        }
+
+        if(reviewDAO.hasReviewed(
+                user.getUser_id(),
+                productId,
+                orderId)){
+            resp.sendRedirect("orders");
+            return;
+        }
+
+        Review review = new Review();
+
+        review.setUser_id(user.getUser_id());
+        review.setProduct_id(productId);
+        review.setOrder_id(orderId);
+        review.setRating(rating);
+        review.setComment(comment);
+
+        reviewDAO.insert(review);
+
+        resp.sendRedirect("orders?status=HOAN_THANH");
     }
 }
